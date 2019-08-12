def05d66ea7c2cea9452b29502ca5fc20bf06817
Wklee96 <lweikang96@gmail.com>
2019-08-12 16:54:46 +0800

    add db

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ app.use(bodyParser.json({}));
 app.post('/fb', function(req, res){
     var id = req.body.entry[0].messaging[0].sender.id;
     var text = req.body.entry[0].messaging[0].message.text;
-    console.log("Post /fb:", JSON.stringify(req.body))
+    console.log("Post /fb:", JSON.stringify(req.body))  
+    // here we add the logic to insert the user data into the database
+    MongoClient.connect(url, function(err, db) {
+      if(err) {
+        console.log(err)
+      }
+      app.findDocument(id, db, function(doc) {
+        if(doc === null){
+          app.initUserPurchase({session:id, Purchase:[]}, db, function(doc){
+            db.close();
+          })
+        }
+      });
+    });
     app.speechHandler(text, id, function(speech){
       app.messageHandler(speech, id, function(result){
         console.log("Async Handled: " + result)
@@ -45,64 +58,64 @@ http.createServer(app).listen(app.get('port'), function(){
 });
 
 app.messageHandler = function(text, id, cb) {
-    var data = {
-      "recipient":{
-          "id":id
-      },
-      "message":{
-          "text":text
-      }
-    };
-    var reqObj = {
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:token},
-      method: 'POST',
-      json: data
-    };
-    console.log("Message Handler:", JSON.stringify(reqObj))
-    request(reqObj, function(error, response, body) {
-      if (error) {
-        //console.log('Error sending message: ', JSON.stringify(error));
-        cb(false)
-      } else if (response.body.error) {
-        //console.log("API Error: " + JSON.stringify(response.body.error));
-        cb(false)
-      } else{
-        cb(true)
-      }
-    });
+  var data = {
+    "recipient":{
+        "id":id
+    },
+    "message":{
+        "text":text
+    }
+  };
+  var reqObj = {
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: data
+  };
+  console.log("Message Handler:", JSON.stringify(reqObj))
+  request(reqObj, function(error, response, body) {
+    if (error) {
+      //console.log('Error sending message: ', JSON.stringify(error));
+      cb(false)
+    } else if (response.body.error) {
+      //console.log("API Error: " + JSON.stringify(response.body.error));
+      cb(false)
+    } else{
+      cb(true)
+    }
+  });
 }
 
 app.speechHandler = function(text, id, cb) {
-    var reqObj = {
-      url: 'https://api.api.ai/v1/query?v=20150910',
-      headers: {
-        "Content-Type":"application/json",
-        "Authorization":"Bearer 4afa6d23ced343df9372d19b59959dd7"
-      },
-      method: 'POST',
-      json: {
-        "query":text,
-        "lang":"en",
-        "sessionId":id
-      }
-    };
-    request(reqObj, function(error, response, body) {
-      if (error) {
-        console.log('Error sending message: ', JSON.stringify(error));
-        cb(false)
-      } else {
-        console.log('Speech Handler Success: ', JSON.stringify(body))
-        cb(body.result.fulfillment.speech);
-      }
-    });
+  var reqObj = {
+    url: 'https://api.api.ai/v1/query?v=20150910',
+    headers: {
+      "Content-Type":"application/json",
+      "Authorization":"Bearer 4afa6d23ced343df9372d19b59959dd7"
+    },
+    method: 'POST',
+    json: {
+      "query":text,
+      "lang":"en",
+      "sessionId":id
+    }
+  };
+  request(reqObj, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', JSON.stringify(error));
+      cb(false)
+    } else {
+      console.log('Speech Handler Success: ', JSON.stringify(body))
+      cb(body.result.fulfillment.speech);
+    }
+  });
 }
 
 app.initUserPurchase = function(data, db, callback) {
-    // Get the documents collection
-    var collection = db.collection('purchase');
-    // Insert some documents
-    collection.insertOne(data, function(err, result) {
+  // Get the documents collection
+  var collection = db.collection('purchase');
+  // Insert some documents
+  collection.insertOne(data, function(err, result) {
 	if (err) throw err;
 	callback(result);
     });
@@ -114,6 +127,6 @@ app.findDocument = function(sessionID, db, callback) {
     // Find some documents
     collection.findOne({'session': sessionID}, function(err, doc) {
         if(err){ throw err; }
-	callback(doc);
+	      callback(doc);
     });
 }
