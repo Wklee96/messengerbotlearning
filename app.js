127d1f43c0b1d7c68ce17da70e0994fcb84d7370
Wklee96 <lweikang96@gmail.com>
2019-08-19 17:15:17 +0800

    added sizing fields

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    https = require('https'),
+    http = require('http'),
     request = require('request'),
     app = express(),
-    token = 'EAAEoEdeyfRABALlxuZCSBKCNaGi1YlJC06bvhSMzzfXSwAfY6FqxlWsqt1NATCeI8imG1wrwuZCUDtdm9ZAb7eFCJn0mZBuIEtaTx0CLenNRXpx1c37mB69WqMxJjrAFnubiZA6u6rDa8oNnqt6aEJ4ljsEIrt4aNZAZAtZBAGtaIwZDZD';
+    fs = require('fs'),
+    token = 'EAAEoEdeyfRABALlxuZCSBKCNaGi1YlJC06bvhSMzzfXSwAfY6FqxlWsqt1NATCeI8imG1wrwuZCUDtdm9ZAb7eFCJn0mZBuIEtaTx0CLenNRXpx1c37mB69WqMxJjrAFnubiZA6u6rDa8oNnqt6aEJ4ljsEIrt4aNZAZAtZBAGtaIwZDZD',
+    ssl0pts = {
+      "key":fs.readFileSync("/etc/letsencrypt/live/bluenode.xyz/privkey.pem"),
+      "cert":fs.readFileSync('/etc/letsencrypt/live/bluenode.xyz/fullchain.pem')
+    }
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb+srv://lweikang96:S9605967a@cluster0-f52b9.mongodb.net/test?retryWrites=true&w=majority';
@@ -17,7 +22,6 @@ app.post('/fb', function(req, res){
   } else {
     var message = req.body.entry[0].messaging[0];
     var id = message.sender.id;
-    //dbInitialisation(id);
     if (message.message) {
       if (message.message.quick_reply) {
         handleInitialResponse(message, id);
@@ -45,37 +49,13 @@ app.get('/health', function(req, res) {
 // set port
 app.set('port', process.env.PORT || 443);
 // start the server
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(ssl0pts, app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 app.messageHandler = function(text, id, cb) {
   if (text === "handover") {
-    var data = {
-      "recipient": {
-        "id": id
-      },
-      "target_app_id": "263902037430900"
-    }
-    var reqObj = {
-      url: 'https://graph.facebook.com/v2.6/me/pass_thread_control',
-      qs: { access_token: token },
-      method: 'POST',
-      json: data
-    };
-    request(reqObj, function (error, response, body) {
-      if (error) {
-        //console.log('Error sending message: ', JSON.stringify(error));
-        cb(false);
-      }
-      else if (response.body.error) {
-        //console.log("API Error: " + JSON.stringify(response.body.error));
-        cb(false);
-      }
-      else {
-        cb(true);
-      }
-    });
+    handOver(id, cb);
   } else {
     sendMessage(id, text, cb);
   }
@@ -169,22 +149,75 @@ app.speechHandler = function(text, id, cb) {
       cb(false)
     } else if (body.result.metadata.isFallbackIntent === "true") {
       console.log('Fallback intent detected, passing protocol')
-      cb(body.result.fulfillment.speech)
+      cb('handover')
     } else {
       console.log('Speech Handler Success: ', JSON.stringify(body))
       var params = body.result.parameters;
-      if (params.productName !== "" && params.cod !== "" && params.address !== "" && params.phone !== "") {
+      console.log('PARAMS', params);
+      if (params.productName !== "" && params.cod !== "" && params.address !== "" && params.phone !== "" && params.CommmoditySize !== "" && params.comments !== "") {
         MongoClient.connect(url, function(err, client) {
-          var db = client.db('purchaseOrder');
-          if (err) {
-            console.log(err);
-          }
-          app.insertPurchase({sessionID: id, productName: params.productName, cod: params.cod, address: params.address, phone: params.phone}, db, function(doc) {
-            client.close();
+          getUsername(id, function(profile) {
+            console.log("Getting profile:", profile);
+            var db = client.db('purchaseOrder');
+            if (err) {
+              console.log(err);
+            }
+            app.insertPurchase({firstName: profile["first_name"], lastName: profile["last_name"], sessionID: id, productName: params.productName, cod: params.cod, address: params.address, phone: params.phone, size: params.CommoditySize, comments: params.comments}, db, function(doc) {
+              console.log("item inserted");
+              client.close();
+            })
           })
         })
       }
       cb(body.result.fulfillment.speech);
+    }
+  });
+}
+
+function getUsername(id, cb) {
+  var reqObj = {
+    url: 'https://graph.facebook.com/v2.6/' + id,
+    qs: {
+      access_token: token,
+      fields: "first_name, last_name"
+    },
+    method: 'GET'
+  };
+  request(reqObj, function(err, res, body) {
+    if (err) {
+      console.log("Profile query error");
+      cb(false);
+    } else {
+      console.log("Profile query success:", body);
+      cb(JSON.parse(body));
+    }
+  })
+}
+
+function handOver(id, cb) {
+  var data = {
+    "recipient": {
+      "id": id
+    },
+    "target_app_id": "263902037430900"
+  };
+  var reqObj = {
+    url: 'https://graph.facebook.com/v2.6/me/pass_thread_control',
+    qs: { access_token: token },
+    method: 'POST',
+    json: data
+  };
+  request(reqObj, function (error, response, body) {
+    if (error) {
+      //console.log('Error sending message: ', JSON.stringify(error));
+      cb(false);
+    }
+    else if (response.body.error) {
+      //console.log("API Error: " + JSON.stringify(response.body.error));
+      cb(false);
+    }
+    else {
+      cb(true);
     }
   });
 }
@@ -258,7 +291,45 @@ function sendMessage(id, text, cb) {
     else {
       cb(true);
     }
-  }), 1500);
+  }), text.length * 100);
+  if (text == "May I know your size?") {
+    sendImage(id, "id1351", cb);
+  }
+}
+
+function sendImage(id, product, cb) {
+  var data = {
+    "recipient": {
+      "id": id
+    },
+    "message": {
+      "attachment": {
+        "type": "image",
+        "payload": {
+          "url": 'https://i.imgur.com/dFMlRcE.png'
+        }
+      }
+    }
+  };
+  var reqObj = {
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: data
+  };
+  request(reqObj, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', JSON.stringify(error));
+      cb(false);
+    }
+    else if (response.body.error) {
+      console.log("API Error: " + JSON.stringify(response.body.error));
+      cb(false);
+    }
+    else {
+      cb(true);
+    }
+  });
 }
 
 function handleResponseFlow(message, id) {
@@ -308,13 +379,3 @@ app.findDocument = function(sessionID, db, callback) {
   })
 }
 
-// app.insertPurchase = function(data, id, db, callback) {
-//   // Get the documents collection
-//   var collection = db.collection('purchase');
-//   // Update document
-//   collection.insertOne({'session': id}, {$set: data}, {upsert: true}, function(err, res) {
-//     if (err) throw err;
-//     console.log("Purchase added", data);
-//     callback(res);
-//   })
-// }
